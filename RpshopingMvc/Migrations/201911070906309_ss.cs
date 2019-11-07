5a9cf080abd0f9d7cc1b41a8c040a7c51b690c02
471394605lzq <471394605@qq.com>
2019-11-07 18:10:05 +0800

    hg

---
+++ RpshopingMvc/Migrations/201911070906309_ss.cs
@@ -0,0 +1,18 @@
+namespace RpshopingMvc.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ss : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.tb_userinfo", "WXOpenid");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.tb_userinfo", "WXOpenid", c => c.String());
+        }
+    }
+}
